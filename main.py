7c47f44daac490d41ef98e8ed0d105aeeb27b829
Seu Nome <seu@email.com>
2024-03-04 21:48:10 -0300

    salvando

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ def obterId():
                 return tarefas_py[-1]['id']
     return 0
 
+def obterIdConcluido():
+    if os.path.isfile('concluido.json') and os.path.getsize('concluido.json'):
+        with open('concluido.json') as c:
+            concluido_py = json.load(c)
+            return concluido_py[-1]['id'] + 1
+    
+    return obterId() 
+
     
 
 
@@ -33,10 +41,14 @@ def obterId():
 @app.route('/')
 def home():
     tarefas_json = []  
+    concluido_json = []
     if os.path.isfile('tarefas.json') and os.path.getsize('tarefas.json') > 0:
         with open('tarefas.json') as tf:
             tarefas_json = json.load(tf)
-    return render_template('index.html',tarefas_json=tarefas_json)
+    if os.path.isfile('concluido.json') and os.path.getsize('concluido.json') > 0:
+        with open('concluido.json') as c:
+            concluido_json = json.load(c)
+    return render_template('index.html',tarefas_json=tarefas_json,concluido_json=concluido_json)
 
 
 @app.route('/adicionar_tarefa',methods=['POST'])
@@ -75,8 +87,10 @@ def concluir_excluir():
    
     
     tarefa_id = int(request.form.get('id_ta'))
+    novo_id = obterIdConcluido()
     tarefa_nome = request.form.get('nome_ta')
     btn = request.form.get('btn')
+    
     
   
 
@@ -85,26 +99,46 @@ def concluir_excluir():
 
     
     if btn == 'excluir':
+        
         with open('tarefas.json') as tf:
                 tarefas_py = json.load(tf)
-                # Criar uma cópia da lista
-                tarefas_copia = tarefas_py[:]
-                for tarefa in tarefas_copia:
+               
+                for tarefa in tarefas_py:
                     if tarefa['id'] == tarefa_id:
+                        print(tarefa['id'])
+                        print(tarefa)
                         tarefas_py.remove(tarefa)
                         flash(f'"{tarefa_nome}" excluido(a) com sucesso')
                         break
     else:
+        tarefa_dict = {
+            'id':novo_id,
+            'nome_tarefa':tarefa_nome
+        }
+        concluido = []
         with open('tarefas.json') as tf:
-            tarefas_py = json.load('tarefas.json')
-            tarefas_copia = tarefas_py[:]
-            for tarefa in tarefas_copia:
+
+            tarefas_py = json.load(tf)
+            for tarefa in tarefas_py:
+                if tarefa['id'] == tarefa_id:
+                    tarefas_py.remove(tarefa)
+        
+        if os.path.isfile('concluido.json') and os.path.getsize('concluido.json') > 0:
+            with open('concluido.json') as c:
+                concluido_py = json.load(c)
+                concluido.extend(concluido_py)
+
+        concluido.append(tarefa_dict)
+        
+        with open('concluido.json','w') as c:
+            json.dump(concluido,c,indent=2)
+        flash(f'"{tarefa_nome}" concluido(a) com sucesso ')
 
 
         
     with open('tarefas.json', 'w') as tf:
-            json.dump(tarefas_py, tf, indent=2)
-    return redirect('/')
+        json.dump(tarefas_py, tf, indent=2)
+        return redirect('/')
 
 
 
